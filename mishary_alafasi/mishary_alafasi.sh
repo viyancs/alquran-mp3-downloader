6c5fe49f8d491b708cd9d8d2055e5fa6deb4e010
sofiyan <sofiyan@detik.com>
2017-10-24 01:59:48 +0700

    adding new receiter mishary_alafasy

---
+++ mishary_alafasi/mishary_alafasi.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#creating at 26/07/2012
+#this script using to dowload alquran mp3 with the reciter is Mishary Alafasy
+#this script will be automatically download all of alquran
+#created by mohamad sofiyani
+
+urls=()
+for (( c=1; c<=114; c++ ))
+	do
+	#creating array for link with 1 number segment
+	if (($c < 10 ))
+ 		then
+        	urls[$c]='http://server8.mp3quran.net/afs/00'$c'.mp3'
+	 	fi
+	 	#creating array for link with 2 number segment
+	 	if (($c >9 && $c<100 ))
+	  		then
+	          	urls[$c]='http://server8.mp3quran.net/afs/0'$c'.mp3'
+		  	fi
+		  	#creating array for link with 3 number segment
+		  	if (($c >99 ))
+		   		then
+		           	urls[$c]='http://server8.mp3quran.net/afs/'$c'.mp3'
+			   	fi
+			   		done
+			   	for f in "${urls[@]}"
+			   		do
+			   		wget "$f"
+done
+
